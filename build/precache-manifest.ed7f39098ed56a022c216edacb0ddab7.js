53e88dcf7b39120e01f5e261e5571a0cf9640d5b
zichenma <zma@liaisionedu.com>
2020-08-12 00:31:53 -0400

    first build

---
+++ build/precache-manifest.ed7f39098ed56a022c216edacb0ddab7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae1e8892b9f7bf6a18fcabbb8dd8640e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8c03082e599e6e6563fa",
+    "url": "/static/css/2.48db8b0f.chunk.css"
+  },
+  {
+    "revision": "8c03082e599e6e6563fa",
+    "url": "/static/js/2.c7f615c9.chunk.js"
+  },
+  {
+    "revision": "8f8309d244192d13f323",
+    "url": "/static/js/main.2aa2f0b4.chunk.js"
+  },
+  {
+    "revision": "2ae464e7456b91110ec7",
+    "url": "/static/js/runtime-main.a0efe8fe.js"
+  }
+]);
